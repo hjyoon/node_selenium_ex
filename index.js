892957b7e717bd51f6999ade31447f7521454fb3
hjyoon <se1620236@naver.com>
2022-03-28 14:41:22 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+// require("chromedriver");
+require("geckodriver");
+
+(async function example() {
+  let driver = await new Builder().forBrowser("firefox").build();
+  try {
+    await driver.get("http://www.google.com/ncr");
+    await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
+    await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
+
+console.log("test");
